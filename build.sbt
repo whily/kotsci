590b6168d639c74dd82e85d0ff337ff4ee4f4ba8
Yujian Zhang <yujian.zhang@gmail.com>
2013-12-28 16:01:11 +0800

    Add skeleton of sbt files and Matrix.scala.

---
+++ build.sbt
@@ -0,0 +1,5 @@
+organization := "net.whily"
+
+name := "scasci"
+
+scalaVersion := "2.10.0"
---
+++ src/main/scala/math/linalg/Matrix.scala
@@ -0,0 +1,21 @@
+/**
+ * Matrix class.
+ *
+ * @author  Yujian Zhang <yujian{dot}zhang[at]gmail(dot)com>
+ *
+ * License: 
+ *   GNU General Public License v2
+ *   http://www.gnu.org/licenses/gpl-2.0.html
+ * Copyright (C) 2013 Yujian Zhang
+ */
+
+package net.whily.scasci.linalg
+
+/** 
+  *  Dense matrix for linear algebra. Vector is treated as a special matrix, i.e. nx1.
+  *  Matrix storage is column major, while index starting from 0.
+  */ 
+class Matrix {
+  val rows: Int = 0
+  val cols: Int = 0
+}
