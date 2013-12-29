b5bc8b13c1da26caad4aac6a59a0bed37a6188aa
Yujian Zhang <yujian.zhang@gmail.com>
2013-12-29 15:21:18 +0800

    Add several basic fields/functions for class Matrix.

--- src/main/scala/math/linalg/Matrix.scala
+++ src/main/scala/math/linalg/Matrix.scala
@@ -9,13 +9,29 @@
  * Copyright (C) 2013 Yujian Zhang
  */
 
-package net.whily.scasci.linalg
+package net.whily.scasci.math.linalg
 
 /** 
-  *  Dense matrix for linear algebra. Vector is treated as a special matrix, i.e. nx1.
-  *  Matrix storage is column major, while index starting from 0.
-  */ 
-class Matrix {
-  val rows: Int = 0
-  val cols: Int = 0
+ * Dense matrix for linear algebra. Vector is treated as a special matrix, i.e. nx1.
+ * Matrix storage is column major, while index starting from 0.
+ *
+ * @param data underlying array data
+ * @param rows number of rows 
+ */ 
+class Matrix[T](val data: Array[T], val rows: Int) {
+  val size: Int = data.length
+
+  val cols: Int = {
+    assert(rows > 0 && (size % rows == 0))
+    data.length / rows
+  }
+
+  val isVector: Boolean = (rows == 1 || cols == 1)
+
+  def apply(index: Int) = {
+    assert(isVector)
+    if (index < 0 || index >= size)
+      throw new IndexOutOfBoundsException(index + " not in [0, " + size + ").")
+    data(index)
+  }
 }
