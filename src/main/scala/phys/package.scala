8776af019b0642e61c000871c3727416d65b6ba2
Yujian Zhang <yujian.zhang@gmail.com>
2014-01-03 23:20:24 +0800

    Add phys/package.scala for physical constants.

---
+++ src/main/scala/phys/package.scala
@@ -0,0 +1,19 @@
+/**
+ * Package file.
+ *
+ * @author  Yujian Zhang <yujian{dot}zhang[at]gmail(dot)com>
+ *
+ * License: 
+ *   GNU General Public License v2
+ *   http://www.gnu.org/licenses/gpl-2.0.html
+ * Copyright (C) 2013 Yujian Zhang
+ */
+
+package net.whily.scasci.phys
+
+package object phys {
+  // Physical constants from http://physics.nist.gov/cuu/Constants/
+  val C = 299792458.0   // Speed of light in vacuum, m / s
+  val G = 6.6738480e-11 // Newtonian constant of gravitation, m^3 / (kg * s^2)
+  val H = 6.6260695729e-34 // Planck constant, J s 
+}
