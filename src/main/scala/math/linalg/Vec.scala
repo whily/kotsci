1ae0b7312ca2be1ae8b60df9d14c45fc9ea54dcc
Yujian Zhang <yujian.zhang@gmail.com>
2014-01-07 21:00:09 +0800

    Format Scaladoc in Vec.scala according to official guideline.

--- src/main/scala/math/linalg/Vec.scala
+++ src/main/scala/math/linalg/Vec.scala
@@ -11,70 +11,89 @@
 
 package net.whily.scasci.math.linalg
 
-/** Specialized 3-element vector. */
+/** Specialized 3-element vector. 
+  * 
+  * It is simpler to use companion object to create Vec3 instances.
+  * 
+  * @param x the 1st component
+  * @param y the 2nd component
+  * @param z the 3rd component
+  */
 class Vec3(var x: Double, var y: Double, var z: Double) {
+  /** Returns a copy of current vector. */
   def copy(): Vec3 = {
     new Vec3(x, y, z)
   }
 
-  // Note that we define quals using mutable fields, which
-  // might cause problems when using hash functions/collections.
-  // Currently hashCode throws exception direclty; if we really need
-  // to support this, we may make Vec3 as immutable (i.e. remove +=, -= methods).
-
+  /** Equals method.
+    * 
+    * Note that we define quals using mutable fields, which
+    * might cause problems when using hash functions/collections.
+    * Currently hashCode throws exception direclty; if we really need
+    * to support this, we may make Vec3 as immutable (i.e. remove +=, -= methods).
+    */
   override def equals(other: Any): Boolean = other match {
     case that: Vec3 => x == that.x && y == that.y && z == that.z
     case _ => false
   }
 
+  /** Hashcode. Disabled to avoid problems with collections. */
   override def hashCode = 
     throw new IllegalArgumentException("Vec3: hashCode not supported.")
 
-  /** Set all elements to `v`. */
+  /** Sets all elements to the input scalar. */
   def fill(v: Double) = {
     x = v
     y = v
     z = v
   }
 
+  /** Unary negate. */
   def unary_- : Vec3 = new Vec3(-x, -y, -z)
 
+  /** In-place plus. */
   def += (that: Vec3) {
     x += that.x
     y += that.y
     z += that.z
   }
 
+  /** Binary plus. */
   def + (that: Vec3): Vec3 = {
     var u = copy()
     u += that
     u
   }
 
+  /** In-place minus. */
   def -= (that: Vec3) {
     x -= that.x
     y -= that.y
     z -= that.z
   }
 
+  /** Binary minus. */
   def - (that: Vec3): Vec3 = {
     var u = copy()
     u -= that
     u
   }
 
+  /** In-place times with a scalar. */
   def *= (a: Double) {
     x *= a
     y *= a
     z *= a
   }
 
+  /** Times with a scalar. */
   def * (a: Double): Vec3 = {
     var u = copy()
     u *= a
     u
   }
 
+  /** In-place division with a scalar. */
   def /= (a: Double) {
     val b = 1.0 / a
     x *= b
@@ -82,23 +101,27 @@ class Vec3(var x: Double, var y: Double, var z: Double) {
     z *= b
   }
 
+  /** Division with a scalar. */
   def / (a: Double): Vec3 = {
     var u = copy()
     u /= a
     u
   }
 
-  /** Dot product. */
+  /** Returns the dot product. */
   def ⋅ (that: Vec3): Double = x * that.x + y * that.y + z * that.z
 
-  /** Cross product, as in http://en.wikipedia.org/wiki/Cross_product */
+  /** Returns the cross product, as in http://en.wikipedia.org/wiki/Cross_product */
   def × (that: Vec3): Vec3 = {
     new Vec3(y * that.z - z * that.y, z * that.x - x * that.z, x * that.y - y * that.x)
   }
 
+  /** Returns the string form of the vector. */
   override def toString = "Vec3(" + x + ", " + y + ", " + z + ")"
 }
 
+/** Factory for [[net.whily.scasci.math.linalg.Vec3]] instance. */
 object Vec3 {
+  /** Creates a three element vector with given components. */
   def apply(x: Double, y: Double, z: Double) = new Vec3(x, y, z)
 }
