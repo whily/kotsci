06fd3c1c2def1df968e8174421e5fd777baab3d7
Yujian Zhang <yujian.zhang@gmail.com>
2015-01-31 20:38:52 +0800

    Move class Point from project history.

---
+++ src/main/scala/geo/Point.scala
@@ -0,0 +1,16 @@
+/**
+ * 2D Point.
+ *
+ * @author  Yujian Zhang <yujian{dot}zhang[at]gmail(dot)com>
+ *
+ * License: 
+ *   GNU General Public License v2
+ *   http://www.gnu.org/licenses/gpl-2.0.html
+ * Copyright (C) 2015 Yujian Zhang
+ */
+
+package net.whily.scasci.geo
+
+/** 2D Point.
+  */
+case class Point(x: Double, y: Double)
